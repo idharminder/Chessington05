a805a8213dad1f5e0b44821451041e88a3f8d806
D K <none>
2023-10-05 20:32:53 +0100

    Red-8 blocked pawns

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,13 +9,19 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         const moves = new Array(0)
+        
         let location = board.findPiece(this)
         let direction = this.player === Player.WHITE ? 1 : -1;
         let startingRow = this.player === Player.WHITE ? 1 : 6;
 
-        moves.push(Square.at(location.row + 1 * direction, location.col))
-        if (location.row === startingRow) {
-            moves.push(Square.at(location.row + 2 * direction, location.col))
+        let potentialSquare1 = Square.at(location.row + 1 * direction, location.col);
+        let potentialSquare2 = Square.at(location.row + 2 * direction, location.col);
+
+        if (!board.getPiece(potentialSquare1)) {
+            moves.push(Square.at(location.row + 1 * direction, location.col))
+            if (location.row === startingRow && !board.getPiece(potentialSquare2)) {
+                moves.push(Square.at(location.row + 2 * direction, location.col))
+            }
         }
         return moves
     }
